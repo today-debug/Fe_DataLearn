14da1a4a621dcf8444b30b388a991b683c8d455a
xueyifan <xueyifan@megvii.com>
2024-06-19 17:17:49 +0800

    distance_utils

--- datalearn/common/utils.py
+++ datalearn/common/utils.py
@@ -1,4 +1,19 @@
-import numpy
+import logging
 
-def mean():
-    return 0
+def get_logger(*, name: str = "DataLearn")->logging.Logger:
+    logger = logging.getLogger(name)
+
+    if getattr(logger, "_init_done__", None):
+        return logger
+    logger._init_done__ = True
+
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    # stdout logging: master only
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
